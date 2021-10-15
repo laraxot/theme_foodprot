c3cd03b899c3ed78e413301895f085d0693811e1
Malebestia <malebestia@yahoo.it>
2021-10-15 19:07:19 +0200

    .

---
+++ Resources/views/livewire/search_address_category/category.blade.php
@@ -0,0 +1,48 @@
+      <div class="row icon-boxes">
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+          <div class="icon-box">
+
+                <img src="{{ Theme::asset('pub_theme::img/svg/food.svg') }}">
+
+            <h4 class="title text-center"><a href="">Cibo</a></h4>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="300">
+          <div class="icon-box">
+                <img src="{{ Theme::asset('pub_theme::img/svg/gifts.svg') }}">
+            <h4 class="title text-center">
+                <a href="">Regali</a>
+            </h4>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="400">
+          <div class="icon-box">
+
+                <img src="{{ Theme::asset('pub_theme::img/svg/shopping.svg') }}">
+
+            <h4 class="title text-center"><a href="">Shopping</a></h4>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="500">
+          <div class="icon-box">
+
+                <img src="{{ Theme::asset('pub_theme::img/svg/markets.svg') }}">
+            <h4 class="title text-center"><a href="">Supermercati</a></h4>
+          </div>
+        </div>
+
+        {{-- 
+            <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="500">
+                <div class="icon-box">
+                    <div class="icon">
+                        <img src="{{ Theme::asset('pub_theme::img/svg/markets.svg') }}"></div>
+                        <h4 class="title"><a href="">Nemo Enim</a></h4>
+                    </div>
+                </div>
+                --}}
+
+
+      </div>
---
+++ Resources/views/restaurants/index/cards.blade.php
@@ -0,0 +1,51 @@
+<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+    <div class="col">
+        <div class="cardmb-4 rounded-3 shadow-sm">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+    </div>
+
+    {{-- 
+    <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+                <h4 class="my-0 fw-normal">Pro</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/mo</small></h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>20 users included</li>
+                    <li>10 GB of storage</li>
+                    <li>Priority email support</li>
+                    <li>Help center access</li>
+                </ul>
+                <button type="button" class="w-100 btn btn-lg btn-primary">Get started</button>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm border-primary">
+            <div class="card-header py-3 text-white bg-primary border-primary">
+                <h4 class="my-0 fw-normal">Enterprise</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>30 users included</li>
+                    <li>15 GB of storage</li>
+                    <li>Phone and email support</li>
+                    <li>Help center access</li>
+                </ul>
+                <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
+            </div>
+        </div>
+    </div>
+    
+    --}}
+    
+</div>
