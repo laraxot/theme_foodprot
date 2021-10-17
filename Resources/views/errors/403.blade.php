a69ae0a337cf25421aa0395fb70240633e2ca41c
Malebestia <malebestia@yahoo.it>
2021-10-18 00:09:30 +0200

    .

---
+++ Resources/views/errors/403.blade.php
@@ -0,0 +1,6 @@
+@extends('pub_theme::errors.minimal')
+
+@section('title', __('Forbidden'))
+@section('code', '403')
+    {{-- @section('message', __($exception->getMessage() ?: 'Forbidden')) --}}
+@section('message', $message ?? __('Forbidden'))
